daf51399e6749ab3462f738db411f5eb53535a48
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-01-21 15:48:35 +0100

    implemented the getTreasures function

--- server/src/main/java/game/logic/LabyrinthServiceImpl.java
+++ server/src/main/java/game/logic/LabyrinthServiceImpl.java
@@ -1,4 +1,13 @@
 package game.logic;
 
+import java.util.List;
+
+import static game.logic.Treasures.getAllTreasures;
+
 public class LabyrinthServiceImpl implements LabyrinthService {
+
+    @Override
+    public List<String> getTreasures() {
+        return getAllTreasures();
+    }
 }
