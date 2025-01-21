b52e91e934559e6bd832400bfe6205a28018a453
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-01-21 15:46:18 +0100

    created a new Treasure object

---
+++ server/src/main/java/game/logic/Treasure.java
@@ -0,0 +1,14 @@
+package game.logic;
+
+public class Treasure {
+
+    private final String name;
+
+    public Treasure(String treasure) {
+        this.name = treasure;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
