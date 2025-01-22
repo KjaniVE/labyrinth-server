71e81b57070d5c38f6334992b9014e269f064402
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-01-22 15:56:22 +0100

    wrote a basic Game class and wrote some tests for it

---
+++ server/src/main/java/game/Game.java
@@ -0,0 +1,41 @@
+package game;
+
+import game.logic.GameId;
+import game.logic.maze.Maze;
+import game.logic.player.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Game {
+
+    private static final int NUMBER_OF_TREASURES_PER_PLAYER = 3;
+
+    private final GameId id;
+    private final Maze maze;
+    private final Set<Player> players = new HashSet<>();
+    private final int maxPlayers;
+
+    public Game(GameId id, int maxPlayers, Player player) {
+        this.id = id;
+        this.maze = new Maze();
+        this.maxPlayers = maxPlayers;
+        players.add(player);
+    }
+
+    public GameId getId() {
+        return id;
+    }
+
+    public Maze getMaze() {
+        return maze;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+}
