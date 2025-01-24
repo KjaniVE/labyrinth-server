ef7a073762b4738ad26e31c024b2ada43643e3dc
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-01-24 17:23:10 +0100

    Get the needed items from the request body

--- server/src/main/java/game/web/views/request/CreateGameRequest.java
+++ server/src/main/java/game/web/views/request/CreateGameRequest.java
@@ -1,9 +1,24 @@
 package game.web.views.request;
 
+import game.logic.GameId;
+import game.logic.player.Player;
+import game.web.LabyrinthUser;
 import io.vertx.ext.web.RoutingContext;
 
 public class CreateGameRequest extends ContextBasedRequestView {
     public CreateGameRequest(RoutingContext ctx) {
         super(ctx);
+    }
+
+    public GameId getGameId() {
+        return new GameId(params.body().getJsonObject().getString("gameName"));
+    }
+
+    public Player getPlayer() {
+        return new Player(new LabyrinthUser(getGameId().toString(), params.body().getJsonObject().getString("playerName")));
+    }
+
+    public int getMaximumPlayers() {
+        return params.body().getJsonObject().getInteger("maximumPlayers");
     }
 }
