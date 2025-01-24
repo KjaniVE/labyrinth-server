1c21249ce5c7a6f3aa327408972b225ccdcbfec7
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-01-24 17:45:46 +0100

    actually creating a game and implementing it

--- server/src/main/java/game/logic/LabyrinthService.java
+++ server/src/main/java/game/logic/LabyrinthService.java
@@ -1,7 +1,11 @@
 package game.logic;
 
+import game.logic.player.Player;
+
 import java.util.List;
 
 public interface LabyrinthService {
     List<String> getTreasures();
+
+    void createGame(GameId gameId, Player player, int maximumPlayers);
 }
--- server/src/main/java/game/logic/LabyrinthServiceImpl.java
+++ server/src/main/java/game/logic/LabyrinthServiceImpl.java
@@ -1,13 +1,27 @@
 package game.logic;
 
+import game.logic.player.Player;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static game.logic.treasure.TreasuresUtil.getAllTreasures;
 
 public class LabyrinthServiceImpl implements LabyrinthService {
 
+    private static final Set<Game> games = new HashSet<>();
+
     @Override
     public List<String> getTreasures() {
         return getAllTreasures();
+    }
+
+    @Override
+    public void createGame(GameId gameId, Player player, int maximumPlayers) {
+        Game game = new Game(gameId, maximumPlayers, player);
+        if (!games.add(game)) {
+            throw new LabyrinthException("Game already exists :(");
+        }
     }
 }
