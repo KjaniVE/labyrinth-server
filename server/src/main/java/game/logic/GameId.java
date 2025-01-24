66ddd1068c44aa4717b4816f9769a38ee8f17115
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-01-24 18:18:16 +0100

    made it so that it can be exported as JSON

--- server/src/main/java/game/logic/GameId.java
+++ server/src/main/java/game/logic/GameId.java
@@ -1,7 +1,14 @@
 package game.logic;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.IOException;
 import java.util.Objects;
 
+@JsonSerialize(using = GameId.GameIdSerializer.class)
 public class GameId {
 
     private final String id;
@@ -26,5 +33,12 @@ public class GameId {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    public static class GameIdSerializer extends JsonSerializer<GameId> {
+        @Override
+        public void serialize(GameId gameId, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString(gameId.id);
+        }
     }
 }
--- server/src/main/java/game/logic/player/PlayerName.java
+++ server/src/main/java/game/logic/player/PlayerName.java
@@ -1,7 +1,14 @@
 package game.logic.player;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.IOException;
 import java.util.Objects;
 
+@JsonSerialize(using = PlayerName.PlayerNameSerializer.class)
 public class PlayerName {
 
     private final String name;
@@ -26,5 +33,12 @@ public class PlayerName {
     @Override
     public int hashCode() {
         return Objects.hash(name);
+    }
+
+    public static class PlayerNameSerializer extends JsonSerializer<PlayerName> {
+        @Override
+        public void serialize(PlayerName playerName, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString(playerName.name);
+        }
     }
 }
