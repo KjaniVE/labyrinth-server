1b1709c6aa7c27fc65419ad86d2d303a9f967281
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-01-24 11:19:12 +0100

    changing the way it checks if it allowed or not since the tiles are not build different

---
+++ server/src/main/java/game/logic/maze/tile/WallConfigUtil.java
@@ -0,0 +1,16 @@
+package game.logic.maze.tile;
+
+import game.Direction;
+
+import java.util.Map;
+
+public class WallConfigUtil {
+
+    protected WallConfigUtil() {
+        throw new UnsupportedOperationException("This class is not instantiable");
+    }
+
+    public static boolean contains(Map<Direction, Wall> walls) {
+        return walls.values().stream().anyMatch(wall -> !wall.exists());
+    }
+}
