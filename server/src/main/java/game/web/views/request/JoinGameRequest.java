167248b3f3329d80c9ccebd96ae7ed08ef2297cd
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2025-01-26 16:22:53 +0100

    get the needed items from the request

--- server/src/main/java/game/web/views/request/JoinGameRequest.java
+++ server/src/main/java/game/web/views/request/JoinGameRequest.java
@@ -1,9 +1,20 @@
 package game.web.views.request;
 
+import game.logic.GameId;
+import game.logic.player.Player;
+import game.web.LabyrinthUser;
 import io.vertx.ext.web.RoutingContext;
 
 public class JoinGameRequest extends ContextBasedRequestView {
     public JoinGameRequest(RoutingContext ctx) {
         super(ctx);
+    }
+
+    public Player getPlayer() {
+        return new Player(new LabyrinthUser(getGameId().toString(), params.pathParameter("playerName").getString()));
+    }
+
+    public GameId getGameId() {
+        return new GameId(params.pathParameter("gameId").getString());
     }
 }
